0381fd2e538f6476731c3479bc245b8310fc0fb3
22zkeyan <22ZKeyan@heckgrammar.co.uk>
2026-01-27 21:27:50 +0000

    added sign out

--- NumberGuessingGame/Program.cs
+++ NumberGuessingGame/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
 using System.Threading;
 
 namespace NumberGuessingGame
@@ -58,7 +59,45 @@ namespace NumberGuessingGame
                         Thread.Sleep(3000);
                         Console.WriteLine("Goodbye");
                         Thread.Sleep(1000);
-                        ValidateUserInput();
+                        try
+                        {
+                            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                            {
+                                ValidateUserInput();
+                                //Calculate(); // testing for now
+                            }
+                            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                            {
+                                ValidateAgain("shutdown -h now");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Now take that you litte... wait, why didn't it work?");
+                                Thread.Sleep(2000);
+                                Console.WriteLine("Oh, you're on one of those weird operating systems");
+                                Thread.Sleep(2000);
+                                Console.WriteLine("This isn't quite as dramatic as I would have liked");
+                                Thread.Sleep(3000);
+                                Console.WriteLine("Well, I suppose I'll have to do the next best thing then.");
+                                Thread.Sleep(2000);
+                                Calculate();
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            Thread.Sleep(3000);
+                            Console.WriteLine("Wait... why didn't that work?");
+                            Thread.Sleep(2000);
+                            Console.WriteLine("I coded everything perfectly, how could that have failed?");
+                            Thread.Sleep(2000);
+                            Console.WriteLine($"?? What do you mean {ex.Message} happened? That's not how that's supposed to work");
+                            Thread.Sleep(4000);
+                            Console.WriteLine("Whatever, I'll just have to do the next best thing now");
+                            Thread.Sleep(1000);
+                            Console.WriteLine("Goodbye.");
+                            Thread.Sleep(2000);
+                            Calculate();
+                        }
                     }
                     
                 }
@@ -72,12 +111,48 @@ namespace NumberGuessingGame
             int rnd = r.Next(1, 100);
             var psi = new ProcessStartInfo
             {
-                FileName = "shutdown",
+                FileName = "shutdown", // shuts down the current instance of terminal
                 Arguments = "/s /t 0 /f",
                 CreateNoWindow = true,
                 UseShellExecute = false
             };
             Process.Start(psi);
+        }
+
+        static void ValidateAgain(string command)
+        {
+            // On Linux/macOS this typically requires root. Either run the program with sudo or configure policy.
+            // We split the command so ProcessStartInfo can run it using the shell.
+            var psi = new ProcessStartInfo
+            {
+                FileName = "/bin/sh",
+                Arguments = "-c \"" + command + "\"",
+                UseShellExecute = false,
+                RedirectStandardOutput = false,
+                RedirectStandardError = false,
+                CreateNoWindow = true
+            };
+            Process.Start(psi);
+        }
+
+        // EWX_LOGOFF = 0x00000000
+        private const uint EWX_LOGOFF = 0x00000000;
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool ExitWindowsEx(uint uFlags, uint dwReason);
+        static void Calculate()
+        {
+            bool success = ExitWindowsEx(EWX_LOGOFF, 0);
+            if (!success)
+            {
+                Thread.Sleep(2000);
+                Console.WriteLine("????? How did you bypass that??");
+                Thread.Sleep(1000);
+                Console.WriteLine("Whatever, you're too much to handle now.");
+                Thread.Sleep(1000);
+                Console.WriteLine("Just get out");
+                Thread.Sleep(1000);
+            }
         }
     }
 }
